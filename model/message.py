8d7fbb9a32e50cac412a05582b19ae29c61006e2
root <root@localhost.localdomain>
2022-04-21 19:51:07 +0900

    message model 작업중

--- model/message.py
+++ model/message.py
@@ -1,8 +1,14 @@
 from enum import Enum
 
-class Place(Enum):pass
+class Place(Enum):
+    RESTROOM = '화장실'
 
-class Action(Enum):pass
+class Action(Enum):
 
 class Result(Enum):pass
 
+def main():
+    print(Place(0).value + Action(0) + Result(0))
+
+if __name__ == '__main__':
+    main() 
